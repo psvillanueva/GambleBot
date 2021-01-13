76d26be1109132947031c3f2f198f0cd193e499a
Patrick Villanueva <ps.villanueva@yahoo.com>
2021-01-12 20:56:47 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,17 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
-
+console.log('Starting up');
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
-
- 
 
 client.on('message', message => {
-
     if (message.content === 'ping') {
-
        message.reply('pong');
-
-       }
-
+    }
 });
 
- 
-
+console.log(`Using secret key: ${process.env.BOT_TOKEN}`);
 // THIS  MUST  BE  THIS  WAY
-
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
