373a46e48f338c1ffb41af7253c9d86d6d69ea17
Patrick Villanueva <ps.villanueva@yahoo.com>
2021-01-12 21:11:37 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,32 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-console.log('Starting up');
+const points = {};
+
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('pong');
+  const tokens = message.content.split(' '); // split on whitespaces
+
+  if (tokens.length && tokens[0].startsWith('!')) {
+    if (message.content.indexOf('gamble') > 1) {
+      message.reply('Fuck me.');
     }
+
+    if (message.author.indexOf('drux7') > 0) {
+      message.reply('SMD');
+    }
+  }
 });
+
+function gamble(user, points) {
+  if (points === 'all') {
+    points[user] = 0;
+    message.reply('You lose');
+  }
+}
 
 console.log(`Using secret key: ${process.env.BOT_TOKEN}`);
 // THIS  MUST  BE  THIS  WAY
